6d1e5a60fc0ef0615e43d2a84843bc76b0678104
Darko <darko.spasojevic33@gmail.com>
2025-02-01 11:13:23 +0100

    dodajem metod uklanjanja proizvoda

--- online_sales_analysis/product_manager.py
+++ online_sales_analysis/product_manager.py
@@ -19,7 +19,15 @@ class ProductManager:
         total = sum(product.price * product.quantity for product in self.products)
         print(f"Ukupna vrednost svih proizvoda: {total:.2f} RSD")
 
-# Test primer
+    def remove_product(self, name: str):
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+                print(f"Proizvod {name} je uklonjen iz sistema.")
+                return
+        print(f"Proizvod {name} nije pronađen.")
+
+
 if __name__ == "__main__":
     pm = ProductManager()
     p1 = Product("Laptop", 75000, 10)
