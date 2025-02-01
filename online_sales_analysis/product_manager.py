da2c2a875178594fcda819599e5bde959540f24f
Darko <darko.spasojevic33@gmail.com>
2025-02-01 12:12:22 +0100

    brisem linije koje prikazuju proizvode i njihove ukupne vrednosti

--- online_sales_analysis/product_manager.py
+++ online_sales_analysis/product_manager.py
@@ -8,17 +8,7 @@ class ProductManager:
         self.products.append(product)
         print(f"Proizvod {product.name} je dodat u sistem.")
 
-    def display_products(self):
-        if not self.products:
-            print("Nema dostupnih proizvoda.")
-        else:
-            for product in self.products:
-                product.display_info()
-
-    def total_value(self):
-        total = sum(product.price * product.quantity for product in self.products)
-        print(f"Ukupna vrednost svih proizvoda: {total:.2f} RSD")
-
+  
     def remove_product(self, name: str):
         for product in self.products:
             if product.name == name:
@@ -31,7 +21,7 @@ class ProductManager:
 if __name__ == "__main__":
     pm = ProductManager()
     p1 = Product("Laptop", 75000, 10)
-    p2 = Product("Telefon", 50000, 5)
+    p2 = Product("Sporet", 530000, 3)
     
     pm.add_product(p1)
     pm.add_product(p2)
